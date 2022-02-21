5d884f68b1aed7e3083c64b9daf9bf59ce3c8799
Emma Harper <emmaharps369@gmail.com>
2022-02-20 21:01:53 -0600

    for runner

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 6a12d5804173897261c5b15c2a35b2bde3e78a82
+AESD_ASSIGNMENTS_VERSION = 6a12d5804173897261c5b15c2a35b2bde3e78a82 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
